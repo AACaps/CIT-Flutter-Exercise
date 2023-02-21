03fee2deb6bd8ea3e2bade4e7f88b3f9cc6d4fc2
AACaps <angeloaugustine.capili@gmail.com>
2023-02-21 23:33:17 +0800

    Upload files in repository

---
+++ app/lib/basic_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'immutable_widget.dart';
+import 'text_layout.dart';
+
+class BasicScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('Welcome to Flutter'),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Icon(Icons.edit),
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 1.0,
+            child: ImmutableWidget(),
+          ),
+          TextLayout()
+        ],
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.lightBlue,
+          child: Center(
+            child: Text("I'm a drawer!"),
+          ),
+        ),
+      ),
+    );
+  }
+}
